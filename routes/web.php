2751f3488e5d8d712186fcdf0634f72110a48471
David Martinez <davidmgilo@gmail.com>
2017-04-18 16:56:27 +0200

    full calendar V - inside app

--- routes/web.php
+++ routes/web.php
@@ -27,5 +27,7 @@ Route::group(['middleware' => 'auth'], function () {
 
     //Please do not remove this if you want adminlte:route and adminlte:link commands to works correctly.
     #adminlte_routes
+    Route::get('calendarlte', 'CalendarlteController@index')->name('calendarlte');
+
 
 });
